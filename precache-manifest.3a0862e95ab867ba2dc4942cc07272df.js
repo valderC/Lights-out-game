dc96649df3b74689aea3597df29006db92120c60
Yurik Valderrama <yurik.valder@gmail.com>
2025-10-18 13:02:35 -0500

    Updates

---
+++ precache-manifest.3a0862e95ab867ba2dc4942cc07272df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94542a81ae5de90701e5",
+    "url": "/Lights-out-game/static/css/main.721d3bc6.chunk.css"
+  },
+  {
+    "revision": "94542a81ae5de90701e5",
+    "url": "/Lights-out-game/static/js/main.94542a81.chunk.js"
+  },
+  {
+    "revision": "a6f3a2216de1c9165847",
+    "url": "/Lights-out-game/static/js/1.a6f3a221.chunk.js"
+  },
+  {
+    "revision": "296c435d7cb0407de778",
+    "url": "/Lights-out-game/static/js/runtime~main.296c435d.js"
+  },
+  {
+    "revision": "8b959174057c297dc455c5a1ec7f66d0",
+    "url": "/Lights-out-game/index.html"
+  }
+];
